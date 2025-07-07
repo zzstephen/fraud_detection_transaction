ef0df5562db2682f4a55fd86525f8394749fe35c
zzstephen <zzstephen@gmail.com>
2025-07-06 22:08:35 -0400

    update

---
+++ data_prep/data_config.py
@@ -0,0 +1,12 @@
+
+
+intermediate = '../../../data/intermediate'
+
+processed_data = '../../../data/processed_data'
+
+model_objects = '../../../model_objects'
+
+filename = '../../../model_objects/geo_encoding.pkl'
+
+time_windw_aggregate_schema = 'time_window_agg.yaml'
+
